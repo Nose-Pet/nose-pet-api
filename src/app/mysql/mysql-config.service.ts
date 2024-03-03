da4fc647293edb97e5e5b7b95562f569734346c7
pjh <jiho5993@naver.com>
2024-03-03 19:46:21 +0900

    feat: entity 추가

--- src/app/mysql/mysql-config.service.ts
+++ src/app/mysql/mysql-config.service.ts
@@ -12,7 +12,7 @@ export class MysqlConfigService implements TypeOrmOptionsFactory {
       username: config.mysqlUsername,
       password: config.mysqlPassword,
       database: config.mysqlDatabase,
-      entities: [entityPath],
+      entities: ['./.yarn/cache/nose-pet-entity-*/node_modules/nose-pet-entity/dist/**/*.entity.js'],
       timezone: 'Z',
       logging: false,
       synchronize: false,
